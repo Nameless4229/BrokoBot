8b419aa6c808e976d8c14f7f180c2020684e3684
Nameless4229 <nameless.4229@gmail.com>
2026-07-30 03:00:20 +0100

    Add files via upload

--- BrokoBot.py
+++ BrokoBot.py
@@ -35,7 +35,7 @@ async def on_message(msg):
 
     #1526833802083569694    
     if autothread:
-        if msg.author.id != bot.user.id and msg.channel.id ==  1532199847048773746:
+        if msg.author.id != bot.user.id and msg.channel.id ==  1526833802083569694:
             await msg.create_thread(name=msg.attachments[0].filename if msg.attachments else msg.content[:50])
             await msg.thread.send(f"Discuss ya bull here:")
 
@@ -50,6 +50,7 @@ async def hello(interaction: discord.Interaction):
     username = interaction.user.mention
     await interaction.response.send_message(f"Sup {username}, I am Broko af my ni- woah I can't say that")
 
+'''
 @bot.tree.command(name="autothread", description="Creates a thread in the current channel for each message sent by a user")
 async def autothread(interaction: discord.Interaction):
     channel = bot.get_channel(interaction.channel_id)
@@ -65,5 +66,6 @@ async def autothread(interaction: discord.Interaction):
             if autothread and msg.channel.id == channel.id:
                 await msg.create_thread(name=msg.attachments[0].filename if msg.attachments else msg.content[:50])
                 await msg.thread.send(f"Discuss ya bull here:")
+'''
 
 bot.run(TOKEN)
